baa5205143ccbd96f70f3216580a26f0e4c831d4
Kacper Wojnowski <kacwoj@student.agh.edu.pl>
2026-03-08 23:12:49 +0100

    fix: enable content-security-policy header

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,16 +8,20 @@ import node from "@astrojs/node";
 import react from "@astrojs/react";
 
 export default defineConfig({
-  output: "server",
-  adapter: node({ mode: "standalone" }),
+  output: 'server',
+  adapter: node({ mode: 'standalone' }),
 
   integrations: [tailwind(), mdx(), sitemap(), icon(), react()],
 
   i18n: {
-    defaultLocale: "en",
-    locales: ["en", "pl"],
+    defaultLocale: 'en',
+    locales: ['en', 'pl'],
     routing: {
-      prefixDefaultLocale: true,
-    },
+      prefixDefaultLocale: true
+    }
   },
+
+  experimental: {
+    csp: true
+  }
 });
